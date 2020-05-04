04c042c4f6303372950f846f758caab8031eb2ff
MartijnSam <martijnsamuels@gmail.com>
2020-05-04 19:49:43 +0200

    cleaned unnc imports

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -6,7 +6,6 @@ import TabContainer from 'react-bootstrap/TabContainer';
 import Row from 'react-bootstrap/Row';
 import Col from "react-bootstrap/Col";
 import Tab from 'react-bootstrap/Tab';
-import Category from "./Category"
  
 
 export default function Categories() {
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState} from "react";
 import Axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
