6beb4e97af2e80768ee56b08f6713195363a1465
MartijnSam <martijnsamuels@gmail.com>
2020-05-06 10:25:49 +0200

    added prettier and styled image

--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -1,150 +1,215 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import { useParams, Link} from "react-router-dom";
-import Image from 'react-bootstrap/Image'
-import Spinner from 'react-bootstrap/Spinner'
-import Tab from 'react-bootstrap/Tab'
+import { useParams, Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import Spinner from "react-bootstrap/Spinner";
+import Tab from "react-bootstrap/Tab";
 
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Badge from 'react-bootstrap/Badge'
-import Table from 'react-bootstrap/Table'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Badge from "react-bootstrap/Badge";
+import Table from "react-bootstrap/Table";
 
 export default function DrinkPage() {
+  const [drink, setDrink] = useState(false);
 
-const [drink, setDrink]= useState(false);
+  const searchDrink = useParams();
+  const searchCategory = searchDrink.category;
 
-const searchDrink = useParams()
-const searchCategory = searchDrink.category
-
-useEffect(() => {async function getDrink(id){
-    console.log(`getting the cocktail:`, id)
-    const data = await Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    console.log(`here is the cocktail data:`, data.data);
-    setDrink(data.data.drinks[0])
-}getDrink(searchDrink.cocktail)}, [searchDrink])
-
-const drinkName = () =>{
-    if (drink === false){
-        return <div>"Getting the drink"<Spinner animation="border"></Spinner></div>
+  useEffect(() => {
+    async function getDrink(id) {
+      console.log(`getting the cocktail:`, id);
+      const data = await Axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      console.log(`here is the cocktail data:`, data.data);
+      setDrink(data.data.drinks[0]);
     }
-    else return <h1>{drink.strDrink}</h1>}
+    getDrink(searchDrink.cocktail);
+  }, [searchDrink]);
 
-const drinkThumb = () =>{
-        if (drink === false){
-            return <div>"Getting the drink"<Spinner animation="border"></Spinner></div>
-        }
-        else return <Image src={drink.strDrinkThumb} fluid />
-  
+  const drinkName = () => {
+    if (drink === false) {
+      return (
+        <div>
+          "Getting the drink"<Spinner animation="border"></Spinner>
+        </div>
+      );
+    } else return <h1>{drink.strDrink}</h1>;
+  };
 
-}
+  const drinkThumb = () => {
+    if (drink === false) {
+      return (
+        <div>
+          "Getting the drink"<Spinner animation="border"></Spinner>
+        </div>
+      );
+    } else return <Image src={drink.strDrinkThumb} fluid />;
+  };
 
-const drinkAlco = () =>{
-    if (drink === false){
-        return <div>"Getting the drink"<Spinner animation="border"></Spinner></div>
+  const drinkAlco = () => {
+    if (drink === false) {
+      return (
+        <div>
+          "Getting the drink"<Spinner animation="border"></Spinner>
+        </div>
+      );
+    } else
+      return (
+        <div>
+          <p>
+            <Badge pill variant="success">
+              {drink.strAlcoholic}
+            </Badge>
+          </p>
+          <p>Glass: {drink.strGlass}</p>
+        </div>
+      );
+  };
+
+  const drinkInstr = () => {
+    if (drink === false) {
+      return (
+        <div>
+          "Getting the drink"<Spinner animation="border"></Spinner>
+        </div>
+      );
+    } else return <div>{drink.strInstructions}</div>;
+  };
+
+  const drinkIngr = () => {
+    if (drink === false) {
+      return (
+        <div>
+          "Getting the drink"<Spinner animation="border"></Spinner>
+        </div>
+      );
     }
-    else return <div><p><Badge pill variant="success">
-    {drink.strAlcoholic}</Badge></p><p>Glass: {drink.strGlass}</p></div>
-
-
-}
-
-
-const drinkInstr = () =>{
-    if (drink === false){
-        return <div>"Getting the drink"<Spinner animation="border"></Spinner></div>
-    }
-    else return <div>{drink.strInstructions}</div>}
-
-    
-const drinkIngr = () =>{
-        
-    if (drink === false){
-        return <div>"Getting the drink"<Spinner animation="border"></Spinner></div>
-    }
-    if (drink.strIngredient1 !== null){
-        return <tbody>
-            <tr><td>{drink.strIngredient1}</td><td>{drink.strMeasure1}</td></tr>
-            <tr><td>{drink.strIngredient2}</td><td>{drink.strMeasure2}</td></tr>
-            <tr><td>{drink.strIngredient3}</td><td>{drink.strMeasure3}</td></tr>
-            <tr><td>{drink.strIngredient4}</td><td>{drink.strMeasure4}</td></tr>
-            <tr><td>{drink.strIngredient5}</td><td>{drink.strMeasure5}</td></tr>
-            <tr><td>{drink.strIngredient6}</td><td>{drink.strMeasure6}</td></tr>
-            <tr><td>{drink.strIngredient7}</td><td>{drink.strMeasure7}</td></tr>
-            <tr><td>{drink.strIngredient8}</td><td>{drink.strMeasure8}</td></tr>
-            <tr><td>{drink.strIngredient9}</td><td>{drink.strMeasure9}</td></tr>
-            <tr><td>{drink.strIngredient10}</td><td>{drink.strMeasure10}</td></tr>
-            <tr><td>{drink.strIngredient11}</td><td>{drink.strMeasure11}</td></tr>
-            <tr><td>{drink.strIngredient12}</td><td>{drink.strMeasure12}</td></tr>
-            <tr><td>{drink.strIngredient13}</td><td>{drink.strMeasure13}</td></tr>
-            <tr><td>{drink.strIngredient14}</td><td>{drink.strMeasure14}</td></tr>
-            <tr><td>{drink.strIngredient15}</td><td>{drink.strMeasure15}</td></tr>           
+    if (drink.strIngredient1 !== null) {
+      return (
+        <tbody>
+          <tr>
+            <td>{drink.strIngredient1}</td>
+            <td>{drink.strMeasure1}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient2}</td>
+            <td>{drink.strMeasure2}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient3}</td>
+            <td>{drink.strMeasure3}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient4}</td>
+            <td>{drink.strMeasure4}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient5}</td>
+            <td>{drink.strMeasure5}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient6}</td>
+            <td>{drink.strMeasure6}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient7}</td>
+            <td>{drink.strMeasure7}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient8}</td>
+            <td>{drink.strMeasure8}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient9}</td>
+            <td>{drink.strMeasure9}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient10}</td>
+            <td>{drink.strMeasure10}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient11}</td>
+            <td>{drink.strMeasure11}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient12}</td>
+            <td>{drink.strMeasure12}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient13}</td>
+            <td>{drink.strMeasure13}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient14}</td>
+            <td>{drink.strMeasure14}</td>
+          </tr>
+          <tr>
+            <td>{drink.strIngredient15}</td>
+            <td>{drink.strMeasure15}</td>
+          </tr>
         </tbody>
-    }}             
-           
-const categoryName = decodeURIComponent(searchCategory)
-let linkCategory = `/categories/${searchCategory}`
+      );
+    }
+  };
 
+  const categoryName = decodeURIComponent(searchCategory);
+  let linkCategory = `/categories/${searchCategory}`;
 
-    return (<div className="drinkpage">
-        <Container>
-            <Row>
-                <Col>{drinkName()}</Col>
-            </Row>
-        </Container>
-            
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row>
-                <Col sm={2}>
-                <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                    <Nav.Link eventKey="first">Instructions</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="second">Ingredients</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="third">Info</Nav.Link>
-                </Nav.Item>
-                </Nav>
-                </Col>
-                <Col sm={4}>
-                <Tab.Content>
-                <Tab.Pane eventKey="first">
-                {drinkInstr()}
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
+  return (
+    <div className="drinkpage">
+      <Container>
+        <Row>
+          <Col>{drinkName()}</Col>
+        </Row>
+      </Container>
+
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col sm={2}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Instructions</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Ingredients</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">Info</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={4}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">{drinkInstr()}</Tab.Pane>
+              <Tab.Pane eventKey="second">
                 <Table striped bordered hover size="sm">
-  <thead>
-    <tr>
-      <th>Ingredients</th>
-      <th>Measures</th>
-    </tr>
-  </thead>
-  {drinkIngr()}
+                  <thead>
+                    <tr>
+                      <th>Ingredients</th>
+                      <th>Measures</th>
+                    </tr>
+                  </thead>
+                  {drinkIngr()}
                 </Table>
-                </Tab.Pane>
-                <Tab.Pane eventKey="third"><p>
-                    <Link to={linkCategory}>
-                <Button variant="outline-success">{categoryName}</Button></Link>
-                </p><p>{drinkAlco()}</p>
-                </Tab.Pane>
-                </Tab.Content>
-                </Col>
-                <Col sm={2}>{drinkThumb()}</Col>
-                </Row>
-                
-                
-               
-                </Tab.Container>
-
-                
-            
-            
-        
-      </div>
-    );
-  }
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+                <p>
+                  <Link to={linkCategory}>
+                    <Button variant="outline-success">{categoryName}</Button>
+                  </Link>
+                </p>
+                <p>{drinkAlco()}</p>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+          <Col sm={4}>{drinkThumb()}</Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  );
+}
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,72 +1,72 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { useParams, Link } from "react-router-dom";
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
- 
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function DrinkPage() {
+  const [search, setSearch] = useState(false);
 
-const [search, setSearch]= useState(false);
+  const searchText = useParams();
+  const searchedFor = decodeURIComponent(searchText.searchtext);
 
-const searchText = useParams()
-const searchedFor = decodeURIComponent(searchText.searchtext)
-
-
-useEffect(() => {async function getSearch(){
-    console.log(`searching for:`, searchText.searchtext)
-    const queryParam = encodeURIComponent(searchText.searchtext)
-    const data = await Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${queryParam}`);
-    console.log(`here is the cocktail data:`, data.data);
-    setSearch(data.data)
-}getSearch()}, [searchText])
-
-const render = () =>{
-    if (search === false){
-        return "Searching for your drink"
+  useEffect(() => {
+    async function getSearch() {
+      console.log(`searching for:`, searchText.searchtext);
+      const queryParam = encodeURIComponent(searchText.searchtext);
+      const data = await Axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${queryParam}`
+      );
+      console.log(`here is the cocktail data:`, data.data);
+      setSearch(data.data);
     }
-    if (search === undefined || typeof search !== "object" || search.drinks === null){
-        return "Found nothing... sorry :("
+    getSearch();
+  }, [searchText]);
+
+  const render = () => {
+    if (search === false) {
+      return "Searching for your drink";
     }
-    else {return search.drinks.map(drink => {
-        let categoryname = encodeURIComponent(drink.strCategory)
-        
-        return  <Col key={drink.idDrink}><Card >
-        <Card.Img variant="top" src={drink.strDrinkThumb}/>
-        <Card.Body>
-            <Card.Title>{drink.strDrink}</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-            <Link to={`/categories/${categoryname}/${drink.idDrink}`}>
-            <Button variant="outline-success">More info
-            </Button></Link>
-            </Card.Footer>
-    </Card></Col>
-    
-        
-        
+    if (
+      search === undefined ||
+      typeof search !== "object" ||
+      search.drinks === null
+    ) {
+      return "Found nothing... sorry :(";
+    } else {
+      return search.drinks.map((drink) => {
+        let categoryname = encodeURIComponent(drink.strCategory);
 
-    })}
+        return (
+          <Col key={drink.idDrink}>
+            <Card>
+              <Card.Img variant="top" src={drink.strDrinkThumb} />
+              <Card.Body>
+                <Card.Title>{drink.strDrink}</Card.Title>
+              </Card.Body>
+              <Card.Footer>
+                <Link to={`/categories/${categoryname}/${drink.idDrink}`}>
+                  <Button variant="outline-success">More info</Button>
+                </Link>
+              </Card.Footer>
+            </Card>
+          </Col>
+        );
+      });
+    }
+  };
 
-}
-
-
-
-
-
-    return (
-      <div className="searchlist">
-        You've searched for "{searchedFor}", here are the results:
-        <Container>
+  return (
+    <div className="searchlist">
+      You've searched for "{searchedFor}", here are the results:
+      <Container>
         <Row xs={2} md={4} lg={6}>
-        {render()}
+          {render()}
         </Row>
-        </Container>
-        
-        
-      </div>
-    );
-  }
+      </Container>
+    </div>
+  );
+}
